bb2b81a38cc75925c06520e21c5d27c581b170ec
Awsy <bass_master89@mail.ru>
2018-11-06 13:52:30 +0200

    source(feat): several fixes in staff route has been modified

--- source/routers/staff.mjs
+++ source/routers/staff.mjs
@@ -5,7 +5,8 @@ import dg from 'debug';
 
 //Instruments
 import { Staff } from '../controllers';
-import { validator } from '../helpers';
+
+// import { authentication, validator } from '../helpers';
 
 const debug = dg('router:staff');
 const router = express.Router();
@@ -34,16 +35,20 @@ router.get('/login', [ limiter ], async (req, res) => {
     }
 });
 
-router.post('/', [ limiter, validator.validate('post', '/staff') ], async (req, res) => {
-    try {
-        const staff = new Staff(req.body);
-        const customer = await staff.create();
-        res.status(201).json(customer);
-    } catch (error) {
-        res.status(400).json({
-            message: error.message,
-        });
-    }
-});
+router.post(
+    '/',
+    [ limiter /*validator.validate('post', '/staff')*/ ],
+    async (req, res) => {
+        try {
+            const staff = new Staff(req.body);
+            const customer = await staff.create();
+            res.status(201).json(customer);
+        } catch (error) {
+            res.status(400).json({
+                message: error.message,
+            });
+        }
+    },
+);
 
 export { router as staff };
